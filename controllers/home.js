23bb82fcb3d4ac3f1d4e8847e165df8d2b9b8d10
Petar <kanev_petar@yahoo.com>
2022-10-02 23:10:49 +0300

    Implemented post functionality

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 
+const { getPosts } = require('../services/post');
+
 router.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
 });
 
+router.get('/catalog', async (req, res) => {
+    const posts = await getPosts();
+    res.render('catalog', { title: 'Catalog', posts });
+});
 
 module.exports = router;
